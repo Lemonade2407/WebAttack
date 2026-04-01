93b52b69865a3705256e240855e377fc4f4836a8
Lemonade <lemonade051112@outlook.com>
2026-04-01 22:09:09 +0800

    Initial commit

---
+++ src/main/java/com/classlab/webattack/WebAttackApplication.java
@@ -0,0 +1,13 @@
+package com.classlab.webattack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAttackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAttackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/classlab/webattack/WebAttackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.classlab.webattack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAttackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
